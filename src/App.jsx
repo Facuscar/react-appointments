5aff8a00f674f27da90bf3c28b29c96975b5081b
Facuscar <facuscardillo@gmail.com>
2022-03-22 19:26:51 -0300

    Finalizamos el local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
 import ListadoPacientes from "./components/ListadoPacientes";
@@ -12,6 +12,19 @@ function App() {
     const pacientesActualizados = pacientes.filter( paciente => paciente.id !== id);
     setPacientes(pacientesActualizados);
   }
+
+  useEffect( () => { 
+    const obtenerLocalStorage = () => {
+      const pacientesLocalStorage  = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+      setPacientes(pacientesLocalStorage);
+    }
+
+    obtenerLocalStorage();
+  }, []);
+
+  useEffect( () => {
+    localStorage.setItem('pacientes', JSON.stringify(pacientes));
+  }, [pacientes]);
 
   return (
     <div className="container mx-auto mt-20">
